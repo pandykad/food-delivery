4a3d6c2fdaed17d8f836f2b6898306b0df10ffad
pandykad <pratikkakade542@gmail.com>
2023-09-29 14:28:05 -0400

    FE craeted. Navbar added

---
+++ food-delivery-fe/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import Home from "./screens/Home";
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+     </> 
+  );
+}
+
+export default App;
